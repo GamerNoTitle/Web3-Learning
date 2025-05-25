d5d92ec999505f672c979e0565230a246418ad0a
GamerNoTitle <GamerNoTitle@outlook.com>
2025-05-25 22:05:02 +0800

    feat: 添加了最基本的加法合约和其函数，用于作为第一个部署的合约进行测试

---
+++ learning/1-Add.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0;
+
+contract TestAdd {
+    function add (uint256 _a, uint256 _b) public pure returns (uint256) {
+        return _a + _b;
+    }
+}
